39a247fe76a02017dd171238c0984fba6adcfef8
thinhtrai1 <ducthinhtrai@gmail.com>
2020-12-22 12:30:15 +0700

    next

--- app/src/main/java/com/chat/services/ChatMessagingBroadcast.kt
+++ app/src/main/java/com/chat/services/ChatMessagingBroadcast.kt
@@ -4,23 +4,76 @@ import android.app.ActivityManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.RemoteInput
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.chat.R
 import com.chat.activities.SplashActivity
+import com.chat.models.Message
+import com.chat.models.User
+import com.chat.services.ChatMessagingService.Companion.KEY_TEXT_REPLY
 import com.chat.utils.Constants
+import com.chat.utils.Utility
+import com.google.gson.Gson
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ChatMessagingBroadcast : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.hasExtra(Constants.EXTRA_ROOM_ID)) {
-            val roomId = intent.getStringExtra(Constants.EXTRA_ROOM_ID)
-            if (isApplicationRunning(context)) {
-                LocalBroadcastManager.getInstance(context)
-                    .sendBroadcast(Intent(Constants.ACTION_OPEN_ROOM).putExtra(Constants.EXTRA_ROOM_ID, roomId))
-            } else {
-                context.startActivity(
-                    Intent(context, SplashActivity::class.java)
-                    .putExtra("roomId", roomId)
-                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+            val roomId = intent.getStringExtra(Constants.EXTRA_ROOM_ID)!!
+            if (!handleReplyIntent(context, intent, roomId)) {
+                if (isApplicationRunning(context)) {
+                    LocalBroadcastManager.getInstance(context)
+                        .sendBroadcast(Intent(Constants.ACTION_OPEN_ROOM).putExtra(Constants.EXTRA_ROOM_ID, roomId))
+                } else {
+                    context.startActivity(
+                        Intent(context, SplashActivity::class.java)
+                            .putExtra("roomId", roomId)
+                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    )
+                }
             }
+        }
+    }
+
+    private fun handleReplyIntent(context: Context, intent: Intent, roomId: String): Boolean {
+        val remoteInput = RemoteInput.getResultsFromIntent(intent) ?: return false
+        NotificationManagerCompat.from(context).cancel(intent.getIntExtra("notificationId", 0))
+        val mediaType: MediaType? = MediaType.parse("text/plain")
+        val reply = remoteInput.getCharSequence(KEY_TEXT_REPLY).toString()
+        val userId = Gson().fromJson(Utility.sharedPreferences.getString(Constants.PREF_USER, ""), User::class.java)?.id.toString()
+        Utility.apiClient.sendMessage(
+            RequestBody.create(mediaType, userId),
+            RequestBody.create(mediaType, roomId),
+            RequestBody.create(mediaType, "0"),
+            RequestBody.create(mediaType, reply),
+            null
+        ).enqueue(object : Callback<Message> {
+            override fun onFailure(call: Call<Message>, t: Throwable) {
+                showToast(context, context.getString(R.string.an_error_occurred_message, t.message))
+            }
+
+            override fun onResponse(call: Call<Message>, response: Response<Message>) {
+                if (response.isSuccessful) {
+                    showToast(context, context.getString(R.string.you_message_has_been_sent))
+                } else {
+                    showToast(context, context.getString(R.string.an_error_occurred_message, response.errorBody()?.string()))
+                }
+            }
+        })
+        return true
+    }
+
+    private fun showToast(context: Context, toast: String) {
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(context, toast, Toast.LENGTH_SHORT).show()
         }
     }
 
--- app/src/main/java/com/chat/services/ChatMessagingService.kt
+++ app/src/main/java/com/chat/services/ChatMessagingService.kt
@@ -4,12 +4,15 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.media.RingtoneManager
 import android.os.Build
 import android.provider.Settings
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.RemoteInput
 import androidx.core.content.ContextCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.chat.R
@@ -23,11 +26,28 @@ import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.net.HttpURLConnection
+import java.net.URL
 
 class ChatMessagingService : FirebaseMessagingService() {
 
     companion object {
         var CURRENT_ROOM_ID = -1
+        const val KEY_TEXT_REPLY = "KEY_TEXT_REPLY"
+    }
+
+    override fun onNewToken(token: String) {
+        Log.e("NEW_FCM_TOKEN", "NEW_FCM_TOKEN: $token")
+        Gson().fromJson(Utility.sharedPreferences.getString(Constants.PREF_USER, ""), User::class.java)?.id?.let {
+            val deviceId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+            Utility.apiClient.updateFireBaseToken(it, deviceId, token).enqueue(object : Callback<ResponseBody> {
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                }
+
+                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                }
+            })
+        }
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
@@ -60,40 +80,38 @@ class ChatMessagingService : FirebaseMessagingService() {
             LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
 
             if (CURRENT_ROOM_ID.toString() != roomId) {
-                showNotification(roomId, id.toInt(), data["notification_title"], data["notification_body"])
+                showNotification(roomId, body, type, id.toInt(), data["notification_title"], data["notification_body"])
             }
         }
     }
 
-    override fun onNewToken(token: String) {
-        Log.e("NEW_FCM_TOKEN", "NEW_FCM_TOKEN: $token")
-        Gson().fromJson(Utility.sharedPreferences.getString(Constants.PREF_USER, ""), User::class.java)?.id?.let {
-            val deviceId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
-            Utility.apiClient.updateFireBaseToken(it, deviceId, token).enqueue(object : Callback<ResponseBody> {
-                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                }
-
-                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                }
-            })
-        }
-    }
-
-    private fun showNotification(roomId: String?, notificationId: Int, notificationTitle: String?, notificationMessage: String?) {
+    private fun showNotification(roomId: String?, message: String?, messageType: String?, notificationId: Int, notificationTitle: String?, notificationMessage: String?) {
         val intent = Intent(this, ChatMessagingBroadcast::class.java)
             .putExtra(Constants.EXTRA_ROOM_ID, roomId)
-        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            .putExtra("notificationId", notificationId)
+        val pendingIntent = PendingIntent.getBroadcast(this, notificationId, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val replyAction: NotificationCompat.Action = NotificationCompat.Action.Builder(R.drawable.ic_send_message, getString(R.string.reply), pendingIntent)
+            .addRemoteInput(RemoteInput.Builder(KEY_TEXT_REPLY).setLabel(getString(R.string.i_love_u)).build())
+            .setAllowGeneratedReplies(true)
+            .build()
 
         val notificationBuilder = NotificationCompat.Builder(this, getString(R.string.notification_channel_id))
             .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(notificationTitle ?: getString(R.string.app_name))
             .setContentText(notificationMessage)
-//            .setStyle(NotificationCompat.BigTextStyle().bigText(notificationMessage))
             .setColor(ContextCompat.getColor(this, R.color.black))
             .setAutoCancel(true)
             .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
             .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
             .setContentIntent(pendingIntent)
+            .addAction(replyAction)
+
+        if (message != null && messageType == "1") {
+            val messageImage = message.getBitmap()
+            notificationBuilder.setLargeIcon(messageImage)
+                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(messageImage).bigLargeIcon(null))
+        }
 
         with(NotificationManagerCompat.from(this)) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -101,6 +119,20 @@ class ChatMessagingService : FirebaseMessagingService() {
                 createNotificationChannel(channel)
             }
             notify(notificationId, notificationBuilder.build())
+        }
+    }
+
+    private fun String.getBitmap(): Bitmap? {
+        return try {
+            val url = URL(Constants.BASE_URL + this)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.connect()
+            val input = connection.inputStream
+            BitmapFactory.decodeStream(input)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
         }
     }
 }
